fa656c3b4124b8e403709c2f8fb4b399183cf00b
maximetourzel <max.tourzel@outlook.fr>
2024-08-01 12:13:35 +0200

    Implements JWT accestoken

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,18 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(email: string, password: string): Promise<any> {
+  async signIn(
+    email: string,
+    password: string,
+  ): Promise<{ accessToken: string }> {
     const user = await this.usersService.findOneByEmail(email);
     if (user?.password !== password) {
       throw new UnauthorizedException();
     }
 
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return user;
+    const payload = { sub: user.id, email: user.email };
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
   }
 }
