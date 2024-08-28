495b3a90ea23a64b87a75fffb40bd5accb58c0aa
maximetourzel <max.tourzel@outlook.fr>
2024-08-29 00:56:18 +0200

    Handle refresh token in interceptor

--- social-net-app/src/app/app.config.ts
+++ social-net-app/src/app/app.config.ts
@@ -5,6 +5,10 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { authReducer } from './store/auth/reducers/auth.reducers';
+import { AuthEffects } from './store/auth/effects/auth.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,8 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideAnimationsAsync(),
     provideHttpClient(
       withInterceptors([
-        authInterceptor
+        authInterceptor,
       ])
-    )
+    ),
+    provideStore({ auth: authReducer}),
+    provideEffects([AuthEffects])
   ]
 };
--- social-net-app/src/app/core/interceptors/auth.interceptor.ts
+++ social-net-app/src/app/core/interceptors/auth.interceptor.ts
@@ -1,32 +1,55 @@
 import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { catchError, concatMap, throwError } from 'rxjs';
+import { catchError, retry, switchMap, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { HttpError } from '../../models';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService)
   const router = inject(Router)
+  let isRefreshing = false
   
   return next(req).pipe(
+    retry(0),
     catchError(error => {
       if (error instanceof HttpErrorResponse) {
+        const HttpError: HttpError = {
+          statusCode: error.error.statusCode,
+          message: error.error.message
+        }
         switch(error.status) {
           case HttpStatusCode.Unauthorized:
-            return authService.getRefreshToken().pipe(
-              concatMap(() => next(req)),
-              catchError(err => {
-                if (err.status === HttpStatusCode.Forbidden) {
+            
+          if (!isRefreshing) {
+            isRefreshing = true;
+      
+            return authService.refreshToken().pipe(
+              switchMap(() => {
+                isRefreshing = false;
+    
+                return next(req);
+              }),
+              catchError((error) => {
+                isRefreshing = false;
+    
+                if (error.status == '403') {
                   authService.logout();
+                  router.navigate(['/login']);
                 }
-                return throwError(() => err)
+    
+                return throwError(() => error);
               })
-            )
+            );
+          }
+          return next(req);
+
           case HttpStatusCode.Forbidden:
             router.navigate(['/forbidden']);
         }
+        return throwError(() => HttpError);
       }
-      return throwError(() => error);
+      return throwError(() => error)
     })
   )
 };
