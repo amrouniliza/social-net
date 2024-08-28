0f2f0d9987f4012dbe19e5fe30a2f18243426697
maximetourzel <max.tourzel@outlook.fr>
2024-08-29 01:01:06 +0200

    Rework auth guard with store

--- social-net-app/src/app/core/guards/auth.guard.ts
+++ social-net-app/src/app/core/guards/auth.guard.ts
@@ -1,25 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service'
-import { map, take } from 'rxjs';
+import { map, take, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  return authService.AuthenticatedUser$.pipe(
-    take(1), // take the first one and then unsubscribe automatically
-    map(user => {
-      // check if route is restricted by role
-      // const { roles } = route.data;
-      // if(user && user.role && roles.includes(user.role.name)) {
-      //  return true;
-      // }
-      if(user) {
-        return true
-      //  return  router.createUrlTree(['/forbidden']);
-      }
-      return  router.createUrlTree(['/login']);
-    })
-  )
+  return authService.isLogged.pipe(
+    take(1),
+    map(isLogged => isLogged ? true : false),
+    tap(isLogged => !isLogged && router.navigate(['/login']))
+  );
 };
