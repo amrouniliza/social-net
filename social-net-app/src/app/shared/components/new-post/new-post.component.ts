07145728f440edc917af9f8f28e0514635b7c8cf
maximetourzel <max.tourzel@outlook.fr>
2024-08-30 17:07:38 +0200

    Rework design of post component

--- social-net-app/src/app/shared/components/new-post/new-post.component.ts
+++ social-net-app/src/app/shared/components/new-post/new-post.component.ts
@@ -1,25 +1,27 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NewPostModalComponent } from './new-post-modal/new-post-modal.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { User } from '../../../models';
+import { CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.scss',
 })
 export class NewPostComponent {
   readonly dialog = inject(MatDialog);
-  user = input.required<User>();
+  @Input()
+  loggedInUser$: Observable<User | null> = of(null);
 
   openNewPostDialog() {
     this.dialog.open<NewPostModalComponent>(NewPostModalComponent, {
       width: '50%',
-      data: this.user(),
     });
   }
 }
